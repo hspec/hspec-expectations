cb46a6836808f951e2924f52b73f270884a96fff
Simon Hengel <sol@typeful.net>
2012-02-02 09:32:48 +0100

    Add `shouldBe` and `shouldThrow`

---
+++ example/Spec.hs
@@ -0,0 +1,44 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+module Spec (main, spec) where
+
+import           Test.Hspec.Monadic
+import           Test.Hspec.HUnit ()
+import           Test.HUnit.ShouldBe
+import           Control.Exception
+
+main = hspec spec
+
+spec = do
+
+  describe "ShouldBe" $ do
+    it "this should succeed" $ do
+      "foo" `shouldBe` "foo"
+
+    it "this should fail" $ do
+      "foo" `shouldBe` "bar"
+
+
+  describe "shouldThrow" $ do
+    it "this should succeed" $ do
+      throw DivideByZero `shouldThrow` (== DivideByZero)
+
+    it "this should succeed" $ do
+      error "foobar" `shouldThrow` anyException
+
+    it "this should succeed" $ do
+      error "foobar" `shouldThrow` anyErrorCall
+
+    it "this should succeed" $ do
+      error "foobar" `shouldThrow` errorCall "foobar"
+
+    it "this should fail" $ do
+      throw Overflow `shouldThrow` (== DivideByZero)
+
+    it "this should fail" $ do
+      return () `shouldThrow` anyException
+
+    it "this should fail" $ do
+      return () `shouldThrow` anyErrorCall
+
+    it "this should fail" $ do
+      error "foo" `shouldThrow` errorCall "foobar"
---
+++ src/Test/HUnit/ShouldBe.hs
@@ -0,0 +1,60 @@
+module Test.HUnit.ShouldBe (
+  shouldBe
+, shouldThrow
+, Selector
+, anyException
+, anyErrorCall
+, errorCall
+, anyIOException
+, anyArithException
+) where
+
+import           Prelude hiding (catch)
+import           Test.HUnit
+import           Control.Exception
+import           Data.Typeable
+import           Control.Arrow ((&&&))
+
+infix 1 `shouldBe`, `shouldThrow`
+
+-- |
+-- @actual \`shouldBe\` expected@ asserts that @actual@ is equal to @expected@
+-- (this is just an alias for `@?=`).
+shouldBe :: (Show a, Eq a) => a -> a -> Assertion
+actual `shouldBe` expected = actual @?= expected
+
+
+type Selector a = (a -> Bool)
+
+shouldThrow :: Exception e => IO a -> (e -> Bool) -> Assertion
+action `shouldThrow` p = do
+  m <- (action >> return Nothing) `catch` (return . Just . (p &&& id))
+  case m of
+    Nothing ->
+      assertFailure msgNothing
+    Just (r, e) ->
+      assertBool (msgFailure e) r
+  where
+    msgNothing = "did not get expected exception: "
+            ++ (show . typeOf . instanceOf $ p)
+
+    msgFailure e = "predicate failed on expected exception: "
+            ++ (show . typeOf $ e) ++ " (" ++ show e ++ ")"
+
+    instanceOf :: Selector a -> a
+    instanceOf _ = error "Test.HUnit.ShouldBe.shouldThrow: brocken Typeable instance"
+
+anyException :: Selector SomeException
+anyException = const True
+
+anyErrorCall :: Selector ErrorCall
+anyErrorCall = const True
+
+errorCall :: String -> Selector ErrorCall
+errorCall s (ErrorCall msg) = s == msg
+
+anyIOException :: Selector IOException
+anyIOException = const True
+
+anyArithException :: Selector ArithException
+anyArithException = const True
