fdfa78ed01a7de1c43aeca725d2cd9951f308088
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-11-21 18:04:39 +0400

    Add `shouldPermute`

--- src/Test/Hspec/Expectations.hs
+++ src/Test/Hspec/Expectations.hs
@@ -8,6 +8,7 @@ module Test.Hspec.Expectations (
 , shouldBe
 , shouldSatisfy
 , shouldContain
+, shouldPermute
 , shouldReturn
 
 -- * Expecting exceptions
@@ -39,7 +40,7 @@ module Test.Hspec.Expectations (
 import           Test.HUnit (Assertion, (@?=), assertBool, assertFailure)
 import           Control.Exception
 import           Data.Typeable
-import           Data.List (isInfixOf)
+import           Data.List ((\\), isInfixOf)
 
 type Expectation = Assertion
 
@@ -67,6 +68,14 @@ shouldContain :: (Show a, Eq a) => [a] -> [a] -> Expectation
 list `shouldContain` sublist = assertBool errorMsg (sublist `isInfixOf` list)
   where
     errorMsg = show list ++ " doesn't contain " ++ show sublist
+
+-- |
+-- @xs \`shouldPermute\` ys@ sets the expectation that @xs@ has the same
+-- elements that @ys@ has, possibly in another order
+shouldPermute :: (Show a, Eq a) => [a] -> [a] -> Expectation
+xs `shouldPermute` ys = assertBool errorMsg (all null [xs \\ ys, ys \\ xs])
+  where
+    errorMsg = show ys ++ " is not a permutation of " ++ show xs
 
 -- |
 -- @action \`shouldReturn\` expected@ sets the expectation that @action@
