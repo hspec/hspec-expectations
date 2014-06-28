1d0b5e281cfa743772412a9626a17da24e2bcbcf
Jeffrey Pigarelli <Jefffrey@users.noreply.github.com>
2014-06-28 18:50:24 +0200

    Added "not" versions to Test.Hspec.Expectations.Contrib

--- src/Test/Hspec/Expectations/Contrib.hs
+++ src/Test/Hspec/Expectations/Contrib.hs
@@ -9,9 +9,15 @@ module Test.Hspec.Expectations.Contrib (
 -- | (useful in combination with `shouldSatisfy`)
 , isLeft
 , isRight
+, shouldNotBe
+, shouldNotSatisfy
+, shouldNotReturn
+, shouldNotContain
 ) where
 
 import Test.Hspec.Expectations
+import Test.HUnit (assertBool)
+import Data.List (isInfixOf)
 
 isLeft :: Either a b -> Bool
 isLeft (Left  _) = True
@@ -20,3 +26,30 @@ isLeft (Right _) = False
 isRight :: Either a b -> Bool
 isRight (Left  _) = False
 isRight (Right _) = True
+
+infix 1 `shouldNotBe`, `shouldNotSatisfy`, `shouldNotContain`, `shouldNotReturn`
+
+-- |
+-- @actual \`shouldNotBe\` notExpected@ sets the expectation that @actual@ is not
+-- equal to @notExpected@
+shouldNotBe :: (Show a, Eq a) => a -> a -> Expectation
+actual `shouldNotBe` notExpected = assertBool ("not expected: " ++ show actual) (actual /= notExpected)
+
+-- |
+-- @v \`shouldNotSatisfy\` p@ sets the expectation that @p v@ is @False@.
+shouldNotSatisfy :: (Show a) => a -> (a -> Bool) -> Expectation
+v `shouldNotSatisfy` p = assertBool ("predicate succeded on: " ++ show v) ((not . p) v)
+
+-- |
+-- @list \`shouldNotContain\` sublist@ sets the expectation that @sublist@ is not
+-- contained anywhere in the second.
+shouldNotContain :: (Show a, Eq a) => [a] -> [a] -> Expectation
+list `shouldNotContain` sublist = assertBool errorMsg ((not . isInfixOf sublist) list)
+  where
+    errorMsg = show list ++ " does contain " ++ show sublist
+
+-- |
+-- @action \`shouldNotReturn\` notExpected@ sets the expectation that @action@
+-- does not return @expected@.
+shouldNotReturn :: (Show a, Eq a) => IO a -> a -> Expectation
+action `shouldNotReturn` notExpected = action >>= (`shouldNotBe` notExpected)
---
+++ test/Test/Utils.hs
@@ -0,0 +1,19 @@
+module Test.Utils where
+
+import           Test.Hspec.HUnit()
+import           Test.Hspec (it)
+import           Test.Hspec.Runner
+import           Test.HUnit
+import           System.IO.Silently
+
+shouldResultIn :: Assertion -> String -> IO ()
+shouldResultIn expectation result = do
+  r <- fmap (last . lines) . capture_ . hspecWith defaultConfig $ do
+    it "" expectation
+  r @?= result
+
+shouldHold :: Assertion -> Assertion
+shouldHold = (`shouldResultIn` "1 example, 0 failures")
+
+shouldNotHold :: Assertion -> Assertion
+shouldNotHold = (`shouldResultIn` "1 example, 1 failure")
