e76217a7fd730de61c736dd171b69a61323cd831
Simon Hengel <sol@typeful.net>
2016-09-08 10:29:57 +0800

    sensei compatibility

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,7 +5,10 @@ import Test.Hspec
 import qualified Test.Hspec.ExpectationsSpec
 import qualified Test.Hspec.Expectations.MatcherSpec
 
-main :: IO ()
-main = hspec $ do
+spec :: Spec
+spec = do
   describe "Test.Hspec.ExpectationsSpec" Test.Hspec.ExpectationsSpec.spec
   describe "Test.Hspec.Expectations.MatcherSpec" Test.Hspec.Expectations.MatcherSpec.spec
+
+main :: IO ()
+main = hspec spec
