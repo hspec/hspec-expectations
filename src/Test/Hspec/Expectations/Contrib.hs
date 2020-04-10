134c3862712ee7f9ad11886e8259a2723c714b50
parsonsmatt <parsonsmatt@gmail.com>
2020-04-10 09:28:01 -0600

    Add the annotate function

--- src/Test/Hspec/Expectations/Contrib.hs
+++ src/Test/Hspec/Expectations/Contrib.hs
@@ -7,8 +7,13 @@ module Test.Hspec.Expectations.Contrib (
 -- | (useful in combination with `shouldSatisfy`)
   isLeft
 , isRight
+
+-- * Annotating expectations
+, annotate
 ) where
 
+import Control.Exception
+import Test.HUnit.Lang (HUnitFailure(..), FailureReason(..))
 
 #if MIN_VERSION_base(4,7,0)
 import Data.Either
@@ -24,3 +29,32 @@ isRight :: Either a b -> Bool
 isRight (Left  _) = False
 isRight (Right _) = True
 #endif
+
+-- |
+-- If you have a test case that has multiple assertions, you can use the
+-- 'annotate' function to provide a string message that will be attached to
+-- the 'Expectation'.
+--
+-- @
+-- describe "annotate" $ do
+--   it "adds the message" $ do
+--     annotate "obvious falsehood" $ do
+--       True `shouldBe` False
+--
+-- ========>
+--
+-- 1) annotate, adds the message
+--       obvious falsehood
+--       expected: False
+--        but got: True
+-- @
+--
+-- @since 0.8.3
+annotate :: String -> IO a -> IO a
+annotate message = handle $ \ (HUnitFailure loc reason) -> throwIO . HUnitFailure loc $ case reason of
+  Reason err -> Reason $ addMessage err
+  ExpectedButGot err expected got -> ExpectedButGot (Just $ maybe message addMessage err) expected got
+  where
+    addMessage err
+      | null err = message
+      | otherwise = message ++ "\n" ++ err
