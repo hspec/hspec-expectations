5d3243df0ae9eb5124a2523a4038914ad1c6e7f2
Nick B <nick@declension.net>
2021-04-28 22:13:02 +0100

    Extra expectations: shouldInclude & shouldIncludeAll
     * Add a new `shouldInclude` (#38 / #35)
     * Also, a more general `shouldIncludeAll` which tests provides subset-like expectations (generalised to any `Foldable`s), with helpful (hopefully) messaging about missing elements
     * Replicate a few unexported helper functions, rather than mess with the overall structure
     * Add some tests around these
     * Update Hpack / Cabal
    
    Closes #38

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,11 +4,13 @@ import Test.Hspec
 
 import qualified Test.Hspec.ExpectationsSpec
 import qualified Test.Hspec.Expectations.MatcherSpec
+import qualified Test.Hspec.Expectations.ContribSpec
 
 spec :: Spec
 spec = do
   describe "Test.Hspec.ExpectationsSpec" Test.Hspec.ExpectationsSpec.spec
   describe "Test.Hspec.Expectations.MatcherSpec" Test.Hspec.Expectations.MatcherSpec.spec
+  describe "Test.Hspec.Expectations.ContribSpec" Test.Hspec.Expectations.ContribSpec.spec
 
 main :: IO ()
 main = hspec spec
